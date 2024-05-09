0a05f7d6b9a8a2f38228bdb21869bd5b05c8f859
Enhmunh-E <enhuush0704@gmail.com>
2024-05-09 17:18:20 +0800

    privacy and terms update

--- app/privacypolicy/page.tsx
+++ app/privacypolicy/page.tsx
@@ -6,64 +6,61 @@ const PrivacyPolicy = () => {
   return (
     <MainLayout>
       <div className="flex flex-1 flex-col justify-center container px-4 md:px-0 md:max-w-[800px] md:mx-auto md:text-lg font-medium  my-4 md:my-16 gap-4">
-        <h1 className="font-bold text-xl">
-          TTR MONGOLIA is an inbound tourism company in Mongolia. Our registered
-          office is at:
-        </h1>
+        <h1 className="font-bold text-2xl">Welcome to TTR Mongolia!</h1>
         <p className="text-black/70">
-          Sukhbaatar District,
+          We are TTR Mongolia ("TTR Mongolia," "we," "us," "our"), deeply
+          committed to safeguarding your privacy and that of your customers.
+          This policy aims to explain our data collection, its purpose, and its
+          usage. We prioritize privacy and assure you that we never sell lists,
+          personal information, or email addresses. Please take a moment to read
+          and understand our Privacy Policy.
+        </p>
+        <h2 className="font-bold text-xl">Information Collection and Usage:</h2>
+        <p className="text-black/70">
+          Personal information refers to data that uniquely identifies or
+          contacts an individual.
+        </p>
+        <h2 className="font-bold text-xl">
+          1. Types of Information Collected:
+        </h2>
+        <p className="text-black/70">
+          When you purchase our products or trips, or use our services, we may
+          collect various details, including your name, address, phone number,
+          email, contact preferences, and credit card information.
+        </p>
+        <h2 className="font-bold text-xl">2. How We Use Your Information:</h2>
+        <p className="text-black/70">
+          We utilize the collected information to keep you informed about our
+          latest product updates, events, and service enhancements.
           <br />
-          Baruun Selbe Street,
+          This data helps us develop, enhance, and personalize our offerings,
+          striving to improve your overall experience.
           <br />
-          1st Khoroo
-          <br />
-          34th apartment 1st Floor.
+          Periodically, we may use your information for essential notifications,
+          like purchase confirmations and policy updates, vital for your
+          interaction with TTR Mongolia.
+        </p>
+        <h2 className="font-bold text-xl">Data Security:</h2>
+        <p className="text-black/70">
+          TTR Mongolia implements comprehensive measures administrative,
+          technical, and physical to protect your personal data against loss,
+          theft, and unauthorized access. Online payment is made through the
+          online secured platform of Golomt Bank.
+        </p>
+        <h2 className="font-bold text-xl">Contact Us:</h2>
+        <p className="text-black/70">
+          For any queries regarding TTR Mongolia's Privacy Policy or data
+          handling, please reach out to us at:
         </p>
         <p className="text-black/70">
-          Before you make a reservation, we encourage you to read these Terms
-          and Conditions carefully, as they constitute a contract agreement
-          between you and TTR Mongolia beginning from your first booking. The
-          person who makes the reservation accepts these conditions on behalf of
-          all participants and responsibility for all payments due.
+          -Email:{" "}
+          <a className="underline" href="mailto:info@ttrmongolia.com">
+            info@ttrmongolia.com
+          </a>
         </p>
-        <h1 className="font-bold text-xl">Car rental service</h1>
         <p className="text-black/70">
-          If you only receive a car rental, 30 days prior to the beginning of
-          the tour you must deposit 50% of the market value of the vehicle.
-          After successful completion of the tour, we will refund your deposit
-          completely. If damage occurs during the tour, any necessary repair
-          fees will be deducted from the deposit before being refunded. It is
-          included following travel utilities: – GPS, backpack, tent, blanket
-        </p>
-        <h1 className="font-bold text-xl">Liability</h1>
-        <p className="text-black/70">
-          After booking on www.ttrmonglia.com, you will need to follow the
-          following terms and legalities. Ti Ti Ar Em Co.Ltd. will not be liable
-          for any damages, incidents, sicknesses, schedule changes, natural
-          disasters caused by weather and road conditions, or any liability for
-          situations beyond the control of Ti Ti Ar Em Co.Ltd.. As the
-          information of www.ttrmonglia.com changes, we have the right to
-          correct any errors that may arise on our website. These general order
-          terms may change and we have the right to renew these requirements
-          without notice. The right to compensation is due to loss or damage to
-          one of two persons due to intentional or unintentional act of the
-          traveler. In principle, the two sides will have to decide on the basis
-          of mutual consent, and the law will be taken into consideration as a
-          result of the law of Mongolia.
-        </p>
-        <h1 className="font-bold text-xl">Insurance</h1>
-        <p className="text-black/70">
-          Please note that individual travel insurance is not included in the
-          tour price and all medical costs and other costs involved must be paid
-          by the client. It is therefore strongly recommended that participants
-          take out personal insurance, especially for the active tours. For
-          example: horse trips, hiking, and snowmobiling.
-        </p>
-        <h1 className="font-bold text-xl">Complaint and request</h1>
-        <p className="text-black/70">
-          Suggestions for your future holiday may be posted on the
-          www.ttrmongolia.com website’s comments section. Please send complaints
-          to ttrmongola@gmail.com and info@ttrmongolia.com.
+          - Address: Ulaanbaatar, Sukhbaatar District, Baruun Selbe Street, 1st
+          Khoroo 34th apartment 1st Floor.
         </p>
         <button className="underline" onClick={() => router.back()}>
           Go Back
--- app/termsandconditions/page.tsx
+++ app/termsandconditions/page.tsx
@@ -6,49 +6,214 @@ const PrivacyPolicy = () => {
   return (
     <MainLayout>
       <div className="flex flex-1 flex-col justify-center container px-4 md:px-0 md:max-w-[800px] md:mx-auto md:text-lg font-medium  my-4 md:my-16 gap-4">
-        <h1 className="font-bold text-xl">Payment term</h1>
+        <h1 className="font-bold text-2xl">Terms and Conditions</h1>
         <p className="text-black/70">
-          TTR Mongolia Chingeltei District,
+          TTR MONGOLIA is an inbound tourism company in Mongolia. Our registered
+          office is at: Sukhbaatar District, Baruun Selbe Street, 1st Khoroo
+          34th apartment 1st Floor.
+        </p>
+        <h2 className="font-bold text-xl">Booking Terms and Conditions</h2>
+        <p className="text-black/70">
+          Before proceeding to book a trip or experience, I hereby confirm that
+          I have carefully read and understood all the terms and conditions
+          outlined below. Should you have any inquiries or require further
+          clarification regarding the information provided, kindly notify us
+          promptly. The person who makes the reservation accepts these
+          conditions on behalf of all participants and responsibility for all
+          payments due.
           <br />
-          Once you have submitted your booking and personal details through
-          www.ttrmongolia.com, you will receive a confirmation and invoice of
-          your booking via e-mail.
+          It is crucial that you thoroughly review and comprehend the booking
+          conditions presented herein. These conditions constitute a legally
+          binding contract between you and Ti Ti Ar Em Co.Ltd operating under
+          the name “TTR Mongolia”, and referenced as such in this agreement. All
+          adventure activities' advertising and promotion are conducted under
+          the brand name "TTR Mongolia".
         </p>
-
-        <h1 className="font-bold text-xl">Refund</h1>
+        <h2 className="font-bold text-xl">Evacuation Responsibility:</h2>
         <p className="text-black/70">
-          Within 29 days or fewer days prior to travel date: 70% of the total
-          price.
-        </p>
-        <p className="text-black/70">
-          30 days prior to travel date: 75% of the total price.
-        </p>
-        <p className="text-black/70">
-          **There are no refunds for sewing or special travel offers.
-        </p>
-        <p className="text-black/70">
-          You may also book and travel within 30 days. If you wish to transfer
-          money via bank account, please notify us at both ttrmongola@gmail.com
-          and info@ttrmongolia.com. Once you have submitted the payment, you
-          will receive an itinerary.
-        </p>
-        <h1 className="font-bold text-xl">Account Information</h1>
-        <p className="text-black/70">
-          Recipient: Ti Ti Ar Em Co.Ltd
+          Acknowledgment of Risk: TTR Mongolia, as a tour operator, conducts
+          activities in areas where accommodation, transportation, safety,
+          hygiene, medical facilities, and other infrastructure standards may
+          occasionally fall below typical expectations. Please consider this
+          when making your reservation. Our activities, especially those
+          involving remote areas, entail risks such as injury, trauma, disease,
+          loss of property, inconvenience, and discomfort. By booking with us,
+          you acknowledge and accept these associated risks to the fullest
+          extent permitted by law.
           <br />
-          Beneficiary account number: 2705118983 / USD /
+          In booking with us, you affirm that your decision to participate in
+          our activities was made after considering all relevant information
+          provided at any given time, and you willingly assume the risks
+          associated with the activities.
           <br />
-          The receiving bank is Golomt Bank
+          WhileTTR Mongolia is committed to providing services with care and
+          expertise, we hold no liability for the actions of others, including
+          guides, transportation and accommodation service providers. This
+          includes any loss, damage, or expense (including loss of prepaid
+          funds) that you may incur as a result of these actions. Additionally,
+          TTR Mongolia is not responsible for customers violating laws in any
+          area visited. Any independent arrangements made by you, not part of
+          the TTR Monglia, are entirely at your own risk. We shall not be held
+          liable for any loss or damage, regardless of the cause (including
+          negligence).
         </p>
+        <h2 className="font-bold text-xl">Pricing:</h2>
         <p className="text-black/70">
-          Payments can be made for the following cards: Visa, MasterCard
+          Our prices are determined based on costs outlined in advertisements by
+          TTR Mongolia. TTR Mongolia reserves the right to adjust prices until
+          payment for your experience is completed. Dates, prices, and
+          itineraries are subject to change and do not encompass flight costs,
+          insurance, or personal purchases during the trip.
         </p>
+        <h2 className="font-bold text-xl">Approval by Trip Commander:</h2>
         <p className="text-black/70">
-          Also, we can offer your feedback on our travel consultant (no car
-          rental or no accommodation). All prices announced for Ti Ti Ar Em
-          Co.Ltd. are USD ($). All payments and refunds are paid in dollars. Ti
-          Ti Ar Em Co.Ltd. is not responsible for changes in exchange rates that
-          may arise from refunds or orders.
+          Throughout all activities, the decision of the trip leader, mentor, or
+          TTR Mongolia representative holds final authority on all matters
+          likely to affect the safety and well-being of the experience or
+          activity. It is imperative to strictly adhere to the laws, customs,
+          guidelines, or regulations of all visited places. Failure to comply
+          with these may result in immediate expulsion from the experience or
+          activity without any entitlement to a refund. Any travel arrangements
+          thereafter will be at your own expense.
+        </p>
+        <h2 className="font-bold text-xl">Age Restrictions:</h2>
+        <p className="text-black/70">
+          Individuals under the age of 18 cannot independently book any
+          activities. They must be accompanied by a responsible adult. TTR
+          Mongolia reserves the right to refuse the participation of any minor
+          they deem unsuitable for a specific activity.
+        </p>
+        <h2 className="font-bold text-xl">Safety Guidelines:</h2>
+        <p className="text-black/70">
+          TTR Mongolia and its service providers cannot be held responsible for
+          customers failing to follow supervising personnel's instructions.
+          Clients participating in activities, supervised or otherwise, do so at
+          their own risk and fully acknowledge the potential risks of injury
+          associated with such activities.
+        </p>
+        <h2 className="font-bold text-xl">Physical Capability and Health:</h2>
+        <p className="text-black/70">
+          Ensuring your physical and mental well-being is of utmost importance.
+          It is your responsibility to disclose any pre-existing medical
+          conditions that might increase your need for medical care during the
+          experience or activity. Additionally, inform us of any conditions that
+          could affect your behavior during the activity or impact your overall
+          enjoyment with fellow participants. If necessary, you may be asked to
+          provide a medical evaluation from a qualified practitioner if
+          requested by our guides, staff at TTR Mongolia. We retain the right to
+          cancel your reservation at our discretion and refund the amount paid
+          by you, deducting any non-refundable costs.
+        </p>
+        <h2 className="font-bold text-xl">Health and Fitness:</h2>
+        <p className="text-black/70">
+          Our experiences often entail moderate physical activity, including
+          walking, hiking, and horse riding, to reach iconic destinations. The
+          intensity generally ranges around moderate, sometimes involving
+          traversing rocky surfaces or terrains with high temperatures. If you
+          have any concerns regarding the physical demands of the experience, we
+          encourage you to discuss your apprehensions with us before confirming
+          your booking. In certain cases where we determine that the experience
+          may not suit your fitness level, we retain the right to cancel your
+          reservation, refunding the paid amount after deducting any
+          non-refundable costs.
+        </p>
+        <h2 className="font-bold text-xl">Complaints:</h2>
+        <p className="text-black/70">
+          Your satisfaction is our priority. Should you have any complaints
+          during your experience or activity, please promptly notify the TTR
+          Mongolia representative. They will take appropriate action to address
+          the issue. If you find their response unsatisfactory, please convey
+          your concerns to us in writing. We are committed to resolving the
+          matter to the best of our ability. In the rare event that your
+          complaint remains unresolved at the conclusion of the trip, please
+          formally notify us in writing within 30 days of trip completion.
+        </p>
+        <h2 className="font-bold text-xl">Publicity:</h2>
+        <p className="text-black/70">
+          By participating, you grant TTR Mongolia the right to use photographs
+          taken during your trip for advertising and promotional purposes
+          through any chosen medium. Your consent allows us to use these images
+          without making specific reference to you and without compensation to
+          you.
+        </p>
+        <h2 className="font-bold text-xl">Privacy and Data Use:</h2>
+        <p className="text-black/70">
+          Your privacy and the security of your personal information are
+          significant to us. Information collected is solely used to tailor our
+          services to your preferences. We may share this information with our
+          trusted service providers to deliver the best experience possible.
+          However, we ensure that your data is not used for any unrelated
+          purposes. Your personal information, including name, address, and
+          phone number, will never be disclosed to external entities without
+          your explicit consent, except when required by law.
+        </p>
+        <h2 className="font-bold text-xl">Legal Compliance:</h2>
+        <p className="text-black/70">
+          This contract is governed by the laws of Mongolia. Any disputes
+          arising from this contract will be under the exclusive jurisdiction of
+          Mongolia courts.
+        </p>
+        <h1 className="font-bold text-2xl mt-8">Cancellation Policy:</h1>
+        <h2 className="font-bold text-xl">Cancellation by TTR Mongolia:</h2>
+        <p className="text-black/70">
+          TTR Mongolia holds the right to cancel activities and will duly inform
+          you of such cancellations. In such cases, we will refund all payments
+          made to TTR Mongolia, deducting any non-refundable costs incurred by
+          our club. However, under any circumstances, we will not be held
+          responsible for any additional costs borne by you.
+          <br />
+          TTR Mongolia will not be liable for any delay, alteration, or
+          cancellation of activities or experiences due to force majeure. Force
+          majeure encompasses circumstances beyond our control, including but
+          not limited to adverse weather conditions, war, threat of war, riot,
+          civil war, terrorist activities, or industrial disruptions.
+        </p>
+        <h2 className="font-bold text-xl">
+          Cancellation by You or Your Representative:
+        </h2>
+        <p className="text-black/70">
+          Cancellation of a reservation or non-participation in a trip will lead
+          to funds being retained by TTR Mongolia as outlined below:
+          <br />
+          - Cancellation 31 days or more before the trip or the experience start
+          date: full amount will be refunded.
+          <br />
+          - Cancellation between 30 to 8 days before the trip or the experience
+          start date: 50% of the experience or trip price will be reserved.
+          <br />
+          - Cancellation less than 8 days before the trip or the experience
+          start date: any amount of payment made is non-refundable and it will
+          not be eligible for use toward any other trips.
+          <br />
+          TTR Mongolia retains the right to terminate anyone's participation in
+          the trip at our discretion and with immediate effect.
+        </p>
+        <h2 className="font-bold text-xl">Payment Terms</h2>
+        <p className="text-black/70">
+          To secure a spot on the trip, you are required to make a payment of
+          50% or the full amount to TTR Mongolia.
+          <br />
+          The final payment of 50% of the total outstanding amount must be
+          settled 30 days before the scheduled departure. Failure to do so will
+          result in the cancellation of your booking, and a refund will not be
+          issued.
+          <br />
+          For trips booked within a week of departure, the full amount must be
+          paid in advance to secure a spot on the trip.
+        </p>
+        <h2 className="font-bold text-xl">
+          Discount Mechanism and Conditions:
+        </h2>
+        <p className="text-black/70">
+          - Discounts are applicable only to bookings made through the TTR
+          Mongolia website.
+          <br />
+          - Discount offers cannot be combined with any other promotions.
+          <br />
+          - The discount must be granted before completing the purchase.
+          <br />
+          - Purchases made during the promotional period are non-refundable.
+          <br />- Other terms and conditions apply.
         </p>
         <button className="underline" onClick={() => router.back()}>
           Go Back
